35b6634ccecc789dcd48f12583c41e9842da67ff
ryoiwata <ryoiwata@github.com>
2025-10-22 09:41:13 -0500

    feat: join for vip and name

---
+++ ut_sr_de_interview.session.sql
@@ -0,0 +1,35 @@
+SELECT 
+    v.id AS vip_id,
+    v.ut_eid,
+    v.created_on AS vip_created_on,
+    v.updated_on AS vip_updated_on,
+    n.first_name,
+    n.middle_name,
+    n.last_name,
+    n.organization_name,
+    n.created_on AS name_created_on,
+    n.updated_on AS name_updated_on
+FROM bio_vip v
+FULL OUTER JOIN bio_name n
+    ON v.id = n.vip_id;
+
+-- SELECT 
+
+
+
+--  FROM bio_vip
+-- FULL OUTER JOIN bio_name 
+-- ON bio_vip.id = bio_name.vip_id
+
+
+
+-- ID,	
+-- FIRST_NAME,
+-- MIDDLE_NAME,
+-- LAST_NAME,
+-- ORGANIZATION_NAME,
+-- VIP_ID,
+-- CREATED_ON,
+-- UPDATED_ON,
+-- CREATED_BY_ID,
+-- UPDATED_BY_ID
