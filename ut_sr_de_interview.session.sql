577bdd2ed80ba9a72a1063b4a1ab0227bb9bcc75
ryoiwata <ryoiwata@github.com>
2025-10-25 11:30:54 -0500

    chore: test

--- ut_sr_de_interview.session.sql
+++ ut_sr_de_interview.session.sql
@@ -1,46 +1,81 @@
 
 
-WITH vip_name_combined AS (
-SELECT 
-    v.id AS vip_id,
-    v.ut_eid,
-    v.created_on AS vip_created_on,
-    v.updated_on AS vip_updated_on,
-    n.first_name,
-    n.middle_name,
-    n.last_name,
-    n.organization_name,
-    n.created_on AS name_created_on,
-    n.updated_on AS name_updated_on
+WITH vip_profiles AS (
+    SELECT 
+        v.id AS vip_id,
+        v.ut_eid,
+        v.entity_id,
+        v.status_id,
+        n.first_name,
+        n.last_name,
+        n.organization_name
+    FROM bio_vip v
+    FULL OUTER JOIN bio_name n
+        ON v.id = n.vip_id
+    ),
 
-FROM bio_vip v
-FULL OUTER JOIN bio_name n
-    ON v.id = n.vip_id
+vip_email_relationships AS (
+    SELECT 
+        vp.*,
+        efv.id AS email_relationship_id,
+        efv.email_id,
+        efv.is_bad AS email_is_bad
+    FROM vip_profiles vp
+    FULL OUTER JOIN bio_email_for_vip efv
+        ON vp.vip_id = efv.vip_id
 ),
-email_combined AS (
-SELECT * FROM vip_name_combined
-FULL OUTER JOIN bio_email_for_vip efv
-    ON vip_name_combined.vip_id = efv.vip_id)
 
-SELECT * FROM email_combined 
-FULL OUTER JOIN bio_email 
-ON email_combined.email_id = bio_email.id
+vip_emails_with_addresses AS (
+    SELECT 
+        ver.*,
+        e.address AS email_address
+    FROM vip_email_relationships ver
+    FULL OUTER JOIN bio_email e
+        ON ver.email_id = e.id
+),
+
+vip_emails_with_tags AS (
+    SELECT 
+        vewa.*,
+        et.id AS tag_id,
+        et.emailtag_id AS email_tag_type
+    FROM vip_emails_with_addresses vewa
+    FULL OUTER JOIN bio_email_for_vip_tags et
+        ON vewa.email_relationship_id = et.emailforvip_id
+)
 
 
+-- more_than_email_CTE AS (
+-- SELECT ut_eid, first_name, last_name, COUNT(*) FROM vip_emails_with_tags
+-- GROUP BY ut_eid, first_name, last_name
+-- HAVING COUNT(*) >= 2
+-- ORDER BY COUNT(*) DESC)
 
---  FROM bio_vip
--- FULL OUTER JOIN bio_name 
--- ON bio_vip.id = bio_name.vip_id
+-- SELECT *, first_name || ' ' || last_name AS full_name FROM more_than_email_CTE
+
+-- SELECT * FROM vip_emails_with_tags
+-- WHERE status_id = 'A'
+-- AND email_is_bad = 0 
+-- CASE 
+--     WHEN email_tag_type = 'PRF' THEN 1
+--     ELSE CASE 
+
+SELECT * FROM vip_emails_with_tags
+WHERE email_address NOT LIKE '%@%'
 
 
-
--- ID,	
--- FIRST_NAME,
--- MIDDLE_NAME,
--- LAST_NAME,
--- ORGANIZATION_NAME,
--- VIP_ID,
--- CREATED_ON,
--- UPDATED_ON,
--- CREATED_BY_ID,
--- UPDATED_BY_ID
+-- SELECT 
+--     vip_id,
+--     ut_eid,
+--     entity_id,
+--     status_id,
+--     first_name,
+--     last_name,
+--     organization_name,
+--     email_relationship_id,
+--     email_id,
+--     email_address,
+--     email_is_bad,
+--     tag_id,
+--     email_tag_type
+-- FROM vip_emails_with_tags
